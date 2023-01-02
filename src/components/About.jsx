e0077d02b4dc98d77866828b573bfd44a8d4424c
shelbinshells <shelbinshells@gmail.com>
2023-01-02 17:28:15 +0530

    Latest update with all details and design

---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Info() {
+    return ( 
+        <div className = "about" >
+            <h2 className="header"> About </h2>   
+            <p className="para"> I am a frontend developer who just started his first React project.
+             And am always interested in learning to improve myself. </p>    
+        </div > 
+        )
+}
---
+++ src/components/Interest.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function Info() {
+    return ( 
+        <div className = "interest" >
+            <h2 className="header"> Interests </h2>   
+            <p className="para"> Food expert.Reader.Internet fanatic.Entrepreneur.Travel geek.Coffee fanatic. </p>  
+        </div> 
+        )
+}
